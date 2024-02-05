1bc5762eb6d7a39624ad56319228bd82d7b3c8f4
BrayanE02 <escb1122@gmail.com>
2024-02-05 11:11:20 -0700

    update

--- src/main/java/com/josh/demo/springstuff/Topic32Application.java
+++ src/main/java/com/josh/demo/springstuff/Topic32Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.josh"})
+@ComponentScan({ "com.josh" })
 @SpringBootApplication
 public class Topic32Application {
-	//changed 
+	// changed
 	public static void main(String[] args) {
 		SpringApplication.run(Topic32Application.class, args);
 	}
--- src/main/java/com/josh/model/UserMapper.java
+++ src/main/java/com/josh/model/UserMapper.java
@@ -5,19 +5,18 @@ import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
 
-public class UserMapper implements RowMapper<NewUserModel>{
+public class UserMapper implements RowMapper<NewUserModel> {
 
     @Override
     public NewUserModel mapRow(ResultSet rs, int rowNum) throws SQLException {
         NewUserModel newLog = new NewUserModel(
-            rs.getLong("ID"),
-            rs.getString("NAME"),
-            rs.getString("LNAME"),
-            rs.getString("EMAIL"),
-            rs.getString("USERNAME"),
-            rs.getString("PASSWORD")
-        );
+                rs.getLong("ID"),
+                rs.getString("NAME"),
+                rs.getString("LNAME"),
+                rs.getString("EMAIL"),
+                rs.getString("USERNAME"),
+                rs.getString("PASSWORD"));
         return newLog;
     }
-    
+
 }
